e1241945336f73a9f14f01c31123a8854fa42e82
jlbuild <juliabuildbot@gmail.com>
2025-06-17 16:25:54 +0000

    GAP_pkg_semigroups_jll build 500.400.1+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -2,11 +2,10 @@
 export semigroups
 
 using GAP_jll
-using GAP_lib_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_semigroups")
 JLLWrappers.@declare_file_product(semigroups)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll)
+    JLLWrappers.@generate_init_header(GAP_jll)
     JLLWrappers.@init_file_product(
         semigroups,
         "lib/gap/semigroups.so",
