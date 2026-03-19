d3a8cb0268f02d94144f35bcc7a9ffab275ac0c6
jlbuild <juliabuildbot@gmail.com>
2026-03-19 00:53:28 +0000

    GAP_pkg_semigroups_jll build 501.50400.1+0

--- src/GAP_pkg_semigroups_jll.jl
+++ src/GAP_pkg_semigroups_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GAP_pkg_semigroups")
-JLLWrappers.@generate_main_file("GAP_pkg_semigroups", UUID("6263fdc2-b0d3-58ac-9933-e1550bf6ed81"))
+JLLWrappers.@generate_main_file("GAP_pkg_semigroups", Base.UUID("6263fdc2-b0d3-58ac-9933-e1550bf6ed81"))
 end  # module GAP_pkg_semigroups_jll
